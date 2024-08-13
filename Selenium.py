53d498050dc7ca1cca38066ce2505af70893d1a9
Utsav247-crypto <57640293+Utsav247-crypto@users.noreply.github.com>
2024-08-13 23:19:14 +0530

    Add files via upload

---
+++ Selenium.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium import webdriver
+
+
+def test_google_search():
+    driver = webdriver.Edge()
+
+    # Open Google
+    driver.get("https://www.google.com")
+
+    # Find the search box and search for "Selenium"
+    search_box = driver.find_element(By.NAME, "q")
+    search_box.send_keys("Selenium")
+    search_box.submit()
+    # Verify the title contains "Selenium"
+    assert "" in driver.title
+
+    # Close the browser
+    driver.quit()
+
+if __name__ == "__main__":
+    test_google_search()
+    
+
